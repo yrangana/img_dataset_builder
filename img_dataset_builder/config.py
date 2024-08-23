9c17773c56b4c840a4702f11f3964a89731b3905
Yasiru Rangana <yrangana@gmail.com>
2024-08-23 16:44:25 +1000

    custom config are allowed

--- img_dataset_builder/config.py
+++ img_dataset_builder/config.py
@@ -1,11 +1,14 @@
 import yaml
 
 
-def load_config(config_file="config.yaml"):
-    with open(config_file, "r" ,encoding='utf-8') as file:
+def load_config(config_file="config.yaml", overrides=None):
+    with open(config_file, 'r', encoding='utf-8') as file:
         config = yaml.safe_load(file)
+    
+    if overrides:
+        config.update(overrides)
+    
     return config
 
-
-# Load the configuration when the module is imported
+# Initialize the configuration
 config = load_config()
